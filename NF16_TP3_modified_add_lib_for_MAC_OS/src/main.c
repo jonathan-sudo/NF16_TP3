347f84be32bb5b7466975c5d0a25082e2343bfc1
Jonathan Legrand <jonathanl@hpjonathan.Home>
2020-10-29 19:31:11 +0100

    suite remplissage listes dans le main

--- NF16_TP3_modified_add_lib_for_MAC_OS/src/main.c
+++ NF16_TP3_modified_add_lib_for_MAC_OS/src/main.c
@@ -8,12 +8,19 @@ int main() {
     T_Patient *listePatients;
     listePatients = malloc(sizeof(listePatients));
     ajouterPatient(listePatients,1,"Viera","Baptiste");
-    ajouterPatient(listePatients,1,"Kowalski","Jan");
+    ajouterPatient(listePatients,2,"Kowalski","Jan");
 
-    T_Patient *listeSoigneurs;
+    T_Soigneur *listeSoigneurs;
     listeSoigneurs = malloc(sizeof(listeSoigneurs));
-    ajouterPatient(listeSoigneurs,1,"Viera","Baptiste");
-    ajouterPatient(listeSoigneurs,1,"Kowalski","Jan");
+    ajouterSoigneur(listeSoigneurs,2,"Legrand","Jonathan");
+    ajouterSoigneur(listeSoigneurs,3,"Vincent","Rémi");
+    
+    T_RendezVous *listeRendezVous;
+    listeRendezVous = malloc(sizeof(listeRendezVous));
+    ajouterRendezVous(listeRendezVous,1,10,11,2,"Rdv test avec Jonathan");
+    ajouterRendezVous(listeRendezVous,1,10,11,3,"Rdv test avec Rémi");
+
+
 
     menuPrincipal();
     return 0;
